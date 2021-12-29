6e11e6561b9be98f9cf8b47a14fc9bc12cb3031d
AdamLisner <90381053+AdamLisner@users.noreply.github.com>
2021-12-29 20:01:51 +0100

    Hop přes potok

--- RP2022/app/src/main/java/com/example/rp2022/MainActivity.java
+++ RP2022/app/src/main/java/com/example/rp2022/MainActivity.java
@@ -7,14 +7,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+
 public class MainActivity extends AppCompatActivity {
 
     Button btnStart;
+    Button goToSignInBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        goToSignInBtn = findViewById(R.id.goToSignInButton);
+        goToSignInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
         btnStart = findViewById(R.id.btnStart);
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,5 +33,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
     }
 }
